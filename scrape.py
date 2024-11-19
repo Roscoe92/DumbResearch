627214c7504c4b3673f78c53f9efe9f005d5c4b9
Maximilian Pappert <maxpappert4292@gmail.com>
2024-11-19 17:46:51 +0000

    ...

--- scrape.py
+++ scrape.py
@@ -28,7 +28,7 @@ def get_driver():
     options = Options()
     # options.add_argument("--disable-gpu")
     # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage")
 
 
     if os.getenv("STREAMLIT_ENV") == "cloud":
