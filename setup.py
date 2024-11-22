31d3bd00798dcdd8d2b4f05cbd674abab2fb23d6
Maximilian Pappert <maxpappert4292@gmail.com>
2024-11-22 22:47:05 +0000

    again...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 with open("requirements.txt") as f:
     content = f.readlines()
-requirements = [x.strip() for x in content if "git+" not in x]
+requirements = [x.strip() for x in content if x.strip() != "-e ." and "git+" not in x]
 
 setup(name='DumbResearch',
       version="0.0.1",
