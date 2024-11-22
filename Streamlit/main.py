45dbc2893cbf77a9cac24bc6fa99d93d2500a7d5
Maximilian Pappert <maxpappert4292@gmail.com>
2024-11-22 23:19:32 +0000

    ....

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -3,10 +3,12 @@ import sys
 import os
 import subprocess
 import shutil
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+import time
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from API.scraper import parse
@@ -59,8 +61,31 @@ def get_webdriver_service(logpath) -> Service:
     )
     return service
 
+def run_selenium(logpath: str, proxy: str, socksStr: str, domain):
+    name = None
+    html_content = None
+    options = get_webdriver_options(proxy=proxy, socksStr=socksStr)
+    service = get_webdriver_service(logpath=logpath)
+    with webdriver.Chrome(options=options, service=service) as driver:
+        url = domain
+        try:
+            driver.get(url)
+            time.sleep(2)
+            # Wait for the element to be rendered:
+            element = WebDriverWait(driver=driver, timeout=10).until(lambda x: x.find_elements(by=By.CSS_SELECTOR, value="h2.eventcard-content-name"))
+            name = element[0].get_property('attributes')[0]['name']
+            html_content = driver.page_source
+        except Exception as e:
+            st.error(body='Selenium Exception occured!', icon='🔥')
+            st.error(body=str(e), icon='🔥')
+        finally:
+            performance_log = driver.get_log('performance')
+            browser_log = driver.get_log('browser')
+    return html_content
+
 if st.button('Parse website'):
-    result = parse(domain)
+    # result = parse(domain)
+    result = run_selenium(domain)
     st.write(result)
 
 # Print the current Python path
