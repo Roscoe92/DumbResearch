9a81d37b2bc1fc86df108c51171bb9713b430c5c
Maximilian Pappert <maxpappert4292@gmail.com>
2024-11-22 23:00:18 +0000

    ....

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import sys
+import os
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from API.scraper import parse
-import os
 
 
 import pandas as pd
