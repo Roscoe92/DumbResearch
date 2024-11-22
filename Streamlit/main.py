07e1a324eab44c3e444c9fb32d7813252d3e1900
Maximilian Pappert <maxpappert4292@gmail.com>
2024-11-22 22:32:04 +0000

    added reference to system path

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from API.scraper import parse
+import os
+import sys
 
 import pandas as pd
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 st.title("Scraper")
 
