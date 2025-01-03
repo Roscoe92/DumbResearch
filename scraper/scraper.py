fb48f46b69cdf67d78deef9bab249196a75d1123
Maximilian Pappert <maxpappert4292@gmail.com>
2025-01-03 23:19:05 +0000

    debugging

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
