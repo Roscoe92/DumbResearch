0f16aa6eeed44429510025f396aa6316560dd57b
Maximilian Pappert <maxpappert4292@gmail.com>
2025-01-05 09:11:36 +0000

    checking if this worked

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,7 @@
 import subprocess
 import shutil
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -27,30 +28,30 @@ def get_chromedriver_version() -> str:
     except Exception as e:
         return str(e)
 
+def get_logpath() -> str:
+    return os.path.join(os.getcwd(), 'selenium.log')
+
 def get_chromedriver_path() -> str:
     return shutil.which('chromedriver')
 
 def get_webdriver_options(headless=False):
     options = Options()
     if headless:
-        options.add_argument("--headless")
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")  # Required for environments with small /dev/shm
-    options.add_argument("--disable-gpu")           # Avoid GPU issues
-    # options.add_argument("--window-size=1920x1080") # Ensure sufficient resolution for rendering
+        options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument('--ignore-certificate-errors')
     return options
 
 
-def get_driver(options):
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ), options=options,)
-
-def get_webdriver_service():
+def get_webdriver_service() -> Service:
     service = Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        )
+        executable_path=get_chromedriver_path()
+    )
     return service
 
 
@@ -61,15 +62,16 @@ def run_selenium(domain, headless=False):
     """
     html_content = None
     options = get_webdriver_options(headless=headless)
-    driver = get_driver(options)
+    service = get_webdriver_service()
 
-    try:
-        driver.get(domain)
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
-        html_content = driver.page_source
-    except Exception as e:
-        print(f"Error occurred while fetching {domain} in headless={headless} mode: {e}")
+    with webdriver.Chrome(options=options, service=service) as driver:
+        try:
+            driver.get(domain)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "body"))
+            )
+            html_content = driver.page_source
+        except Exception as e:
+            print(f"Error occurred while fetching {domain} in headless={headless} mode: {e}")
 
     return html_content
